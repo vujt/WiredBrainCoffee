8fe241dbdd3f9df55804b63a402d0bc15cb7d1d5
John Vu <vujt@hotmail.com>
2019-06-13 15:05:47 -0700

    add quit

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -9,13 +9,19 @@ namespace WiredBrainCoffee.ShopInfoTool
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
-            Console.WriteLine("Write 'help' to list available coffee shop commands");
+            Console.WriteLine("Write 'help' to list available coffee shop commands, " +
+                "write 'quit' to exit application");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
             while (true)
             {
                 var line = Console.ReadLine();
+
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
 
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
 
